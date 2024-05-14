ab4924aeb629ddb3b3ddc906611f59230fd73f84
Francisco Contreras <fcocc77@gmail.com>
2024-05-14 16:34:57 -0400

    intrinsic_nuke: init

---
+++ intrinsic_nuke.py
@@ -0,0 +1,45 @@
+import torch
+from altered_midas.midas_net import MidasNet
+from altered_midas.midas_net_custom import MidasNet_small
+
+weights = './final_weights.pt'
+
+
+class combine(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        device = 'cuda'
+
+        combined_dict = torch.load(weights)
+        iid_state_dict = combined_dict['iid_state_dict']
+
+        self.real_model = MidasNet_small(
+            exportable=False, input_channels=5, output_channels=1)
+        self.real_model.load_state_dict(iid_state_dict)
+        self.real_model = self.real_model.to(device)
+
+    def forward(self, combined):
+        return self.real_model(combined)
+
+
+class ordinal(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        device = 'cuda'
+
+        combined_dict = torch.load(weights)
+        ord_state_dict = combined_dict['ord_state_dict']
+
+        self.ord_model = MidasNet()
+        self.ord_model.load_state_dict(ord_state_dict)
+        self.ord_model = self.ord_model.to(device)
+
+    def forward(self, img):
+        return self.ord_model(img)
+
+
+combine_script = torch.jit.script(combine().eval())
+combine_script.save('./nuke/Cattery/Intrinsic/COMBINE.pt')
+
+ordinal_script = torch.jit.script(ordinal().eval())
+ordinal_script.save('./nuke/Cattery/Intrinsic/ORDINAL.pt')
